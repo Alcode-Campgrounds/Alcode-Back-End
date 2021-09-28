83e3078038ee1f9b352a51d25487eac50ec43e78
TANNER MECK <steve.meck246@gmail.com>
2021-09-28 13:51:42 -0700

    getAll test setup and failing

--- __tests__/bender.test.js
+++ __tests__/bender.test.js
@@ -7,15 +7,34 @@ describe('demo routes for weight', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  beforeEach(async () => {
+    const quotes = [{ character: 'bender' }, { character: 'bender' }];
+    await Promise.all(
+      quotes.map(quote => {
+        return request(app)
+          .post('/api/bender/quote')
+          .send(quote);
+      })
+    );
+  });
 
-  it('should create a new quote from bender using POST / route', async () => {
+  it('should create a new quote from bender using POST / route', () => {
     return request(app)
       .post('/api/bender/quote')
       .send({ character: 'bender' })
       .then(res => {
-        expect(res.body).toEqual({ id: 1, character: 'bender', quote: expect.any(String) });
+        expect(res.body).toEqual({ id: expect.any(Number), character: 'bender', quote: expect.any(String) });
       });
-      
+  });
+  it('should get all quotes from bender using GET / route', async () => {
+    await request(app)
+      .post('/api/bender/quote')
+      .send({ character: 'bender' });
+    return request(app)
+      .get('/api/bender/quote')
+      .then(res => {
+        expect(res.body).toEqual([{ id: 1, character: 'bender', quote: expect.any(String) }, { id: 2, character: 'bender', quote: expect.any(String) }, { id: 3, character: 'bender', quote: expect.any(String) }]);
+      });
   });
   afterAll(() => {
     pool.end();
