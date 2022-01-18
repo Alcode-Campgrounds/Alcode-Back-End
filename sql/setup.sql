b04f2dfee9d2f8b2405ce2ed24094b48f93b1349
satyadvaya <satyadvaya@gmail.com>
2022-01-18 12:46:59 -0800

    resolving merge conflict from pull

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS images CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,4 +8,28 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
+
+CREATE TABLE favorites (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_email TEXT NOT NULL,
+    FOREIGN KEY (user_email) REFERENCES users(email),
+    facility_id TEXT NOT NULL UNIQUE,
+    facility_name TEXT NOT NULL,
+    facility_description TEXT,
+    facility_directions TEXT,
+    facility_phone TEXT,
+    facility_email TEXT,
+    reservable BOOLEAN 
+);
+
+CREATE TABLE images (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    favorites_facility_id TEXT NOT NULL,
+    FOREIGN KEY (favorites_facility_id) REFERENCES favorites(facility_id),
+    url TEXT NOT NULL
+);
+
+
+
+
 
