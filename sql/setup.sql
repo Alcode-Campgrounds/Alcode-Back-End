f0dc7fd461d14b7b9bec0fdafc7f22aea221b214
Cristian Montes <cmontesmujica@gmail.com>
2022-01-17 23:30:51 -0700

    tables favorites and images created and main logic to post a new favorite

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS images CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,4 +8,27 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
+
+CREATE TABLE favorites (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    facility_id TEXT NOT NULL UNIQUE,
+    facility_name TEXT NOT NULL,
+    facility_description TEXT NOT NULL,
+    facility_directions TEXT NOT NULL,
+    facility_phone TEXT NOT NULL,
+    facility_email TEXT,
+    reservable BOOLEAN NOT NULL
+);
+
+CREATE TABLE images (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    favorites_facility_id TEXT NOT NULL REFERENCES favorites(facility_id),
+    url TEXT NOT NULL
+)
+
+
+
+
+
 
